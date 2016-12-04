ae25c39036dd2427d1981f62301eba3b199946fd
Nicholas Leskiw <nleskiw@groupon.com>
2016-12-04 12:57:37 -0600

    First basic implementation
    Allows shuffling with cryptographic-level randomness.
    Uses UTF-8 symbols for suits.

--- goplaycards.go
+++ goplaycards.go
@@ -1,42 +1,48 @@
-package goplaycards
+package main
 
-type Suit struct{ name, symbol string }
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+)
 
-type Suits struct {
-	Club    Suit
-	Diamond Suit
-	Heart   Suit
-	Spade   Suit
-}
+type Suit struct{ Name, Symbol string }
 
-type FaceValue struct {
+type Facevalue struct {
 	Name  string
 	Value int
 }
 
-type Card interface {
-	greater_than(*Card) (ans bool)
-	less_than(*Card) (ans bool)
-	equal(*Card) (ans bool)
-	facecard(*Card) (ans bool)
-}
-
 type Card struct {
 	Suit  Suit
-	Value Value
+	Value Facevalue
 }
 
-type Deck interface {
-	(d Deck) Shuffle(count int) (err error)
-	(d Deck) Draw(count int) (cards []Card, err error)
-	(d Deck) Initialize(*Deck ) (err error)
+func (card_a *Card) greater_than(card_b *Card) bool {
+	return card_a.Value.Value > card_b.Value.Value
+}
+
+func (card_a *Card) less_than(card_b *Card) bool {
+	return card_a.Value.Value < card_b.Value.Value
+}
+
+func (card_a *Card) equal(card_b *Card) bool {
+	return card_a.Value.Value == card_b.Value.Value
+}
+
+func (card *Card) facecard() (ans bool) {
+	n := card.Value.Name
+	return n == "Jack" || n == "Queen" || n == "King" || n == "Ace"
 }
 
 type Deck struct {
 	Cards []Card
 }
 
-func Initialize(*Deck) error {
+func (d *Deck) Initialize() error {
+	// TODO: Empty Deck before initialization
+	// Ensure cards past 52 are deleted.
+
 	suits := []Suit{
 		Suit{"Clubs", "♣"},
 		Suit{"Diamonds", "♦"},
@@ -44,25 +50,68 @@ func Initialize(*Deck) error {
 		Suit{"Spades", "♠"},
 	}
 	facevalues := []Facevalue{
-		FaceValue{"Two", 2},
-		FaceValue{"Three", 3},
-		FaceValue{"Four", 4},
-		FaceValue{"Five", 5},
-		FaceValue{"Six", 6},
-		FaceValue{"Seven", 7},
-		FaceValue{"Eight", 8},
-		FaceValue{"Nine", 9},
-		FaceValue{"Ten", 10},
-		FaceValue{"Jack", 11},
-		FaceValue{"Queen", 12},
-		FaceValue{"King", 13},
-		FaceValue{"Ace", 14},
+		Facevalue{"Two", 2},
+		Facevalue{"Three", 3},
+		Facevalue{"Four", 4},
+		Facevalue{"Five", 5},
+		Facevalue{"Six", 6},
+		Facevalue{"Seven", 7},
+		Facevalue{"Eight", 8},
+		Facevalue{"Nine", 9},
+		Facevalue{"Ten", 10},
+		Facevalue{"Jack", 11},
+		Facevalue{"Queen", 12},
+		Facevalue{"King", 13},
+		Facevalue{"Ace", 14},
 	}
 
-	for _, suit := range suits { 
-		for _, facevalue := range  facevalues{
-			
+	for _, suit := range suits {
+		for _, facevalue := range facevalues {
+			d.Cards = append(d.Cards, Card{Suit: suit, Value: facevalue})
 		}
 	}
+	d.Shuffle()
+	return nil
+}
 
+func (d *Deck) Shuffle() (err error) {
+	var old []Card
+	old = d.Cards
+	var shuffled []Card
+	for i := len(old); i > 0; i-- {
+		nBig, e := rand.Int(rand.Reader, big.NewInt(int64(i)))
+		if e != nil {
+			panic(e)
+		}
+		j := nBig.Int64()
+		shuffled = append(shuffled, old[j])
+		old = remove(old, j)
+	}
+	d.Cards = shuffled
+	return nil
+}
+
+func remove(slice []Card, i int64) []Card {
+	copy(slice[i:], slice[i+1:])
+	return slice[:len(slice)-1]
+}
+
+func (d *Deck) Draw(count int) (cards []Card, err error) {
+	// TODO: Implement this function
+	return nil, nil
+}
+
+func main() {
+	var d Deck
+	d.Initialize()
+	for i := 5; i > 0; i-- {
+		fmt.Printf("First Card: %+v\n", d.Cards[0])
+		fmt.Printf("length of deck: %d\n", len(d.Cards))
+		fmt.Printf("Shuffing...\n")
+		e := d.Shuffle()
+		if e != nil {
+			panic(e)
+		}
+	}
+	return
 }
